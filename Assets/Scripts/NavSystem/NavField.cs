89a1f202982f228805f66a0d52aa01fc24ba1b31
MeduPP <parfencenkoartem@gmail.com>
2023-02-04 20:14:30 +0300

    field generator fixes

--- Assets/Scripts/NavSystem/NavField.cs
+++ Assets/Scripts/NavSystem/NavField.cs
@@ -5,70 +5,131 @@ namespace NavSystem.Field
 {
     public class NavField : MonoBehaviour
     {
-        [SerializeField] private RectTransform _gameArea;
         [SerializeField] private GameObject _cellPrefab;
-        [SerializeField] private Camera _gameCamera;
-        [SerializeField] private int _layersCount;
+        [SerializeField] private Vector2 _leftUpCorner;
+        [SerializeField] private Vector2 _rightDownCorner;
+        [SerializeField] private Vector2 _offset;
 
         [ContextMenu("Generate field")]
-        public void SetUpField()
+        public void GenerateField()
         {
-            Vector2 fieldSize = _gameArea.rect.size / 2;
-            Vector2 origin = _gameCamera.WorldToScreenPoint(_gameArea.position);
+            List<GameObject> cells = new List<GameObject>();
 
-            //Right up angle
-            Vector2 RightUpPos = _gameCamera.ScreenToWorldPoint(origin + fieldSize);
-            //Left down angle
-            Vector2 LeftDownPos = _gameCamera.ScreenToWorldPoint(origin - fieldSize);
+            Vector2 cellScale = (Vector2)_cellPrefab.transform.lossyScale;
 
-            Vector2 cellSize = _gameCamera.ScreenToWorldPoint((Vector2)_cellPrefab.transform.localScale
-                             + new Vector2(_gameCamera.pixelWidth, _gameCamera.pixelHeight) / 2);
+            if (_offset.y <= -cellScale.y)
+                _offset.y = -cellScale.y + cellScale.x / 3;
 
-            Vector2 offset = cellSize / _layersCount;
+            if (_offset.x <= -cellScale.x)
+                _offset.x = -cellScale.x + cellScale.x / 3;
 
-            List<GameObject> rows = new();
-            List<GameObject> columns = new();
+            Vector2 offsetScale = cellScale + _offset;
 
-            for (int i = 0; i < _layersCount; i++)
-            {
-                int cellCountInRow = Mathf.FloorToInt((_gameArea.rect.width - _cellPrefab.transform.localScale.x/_layersCount * i) / _cellPrefab.transform.localScale.x);
-                int cellCountInColumn = Mathf.FloorToInt((_gameArea.rect.height - _cellPrefab.transform.localScale.y / _layersCount * i) / _cellPrefab.transform.localScale.y);
+            Vector2 rightUpCorner = new Vector2(_rightDownCorner.x, _leftUpCorner.y);
+            Vector2 leftDownCorner = new Vector2(_leftUpCorner.x, _rightDownCorner.y);
 
-                List<GameObject> currentRows = new();
-                currentRows.AddRange(DrowRow(new Vector2(LeftDownPos.x + offset.x * i, RightUpPos.y), cellSize, cellCountInRow, IsReverse: false));
-                currentRows.AddRange(DrowRow(RightUpPos - new Vector2(offset.x * i, 0), cellSize, cellCountInRow, IsReverse: true));
-                rows.AddRange(currentRows);
 
-                //Dorow columns 
-                foreach (var cell in currentRows)
+            int cellCountInRow = Mathf.FloorToInt((_rightDownCorner.x - _leftUpCorner.x) / offsetScale.x);
+            int cellCountInColumn = Mathf.FloorToInt((_leftUpCorner.y - _rightDownCorner.y) / offsetScale.y);
+
+            if(_offset.x < 0)
+                cellCountInRow--;
+            if (_offset.y < 0)
+                cellCountInColumn--;
+
+            Debug.Log(_rightDownCorner.x - _leftUpCorner.x);
+            Debug.Log(offsetScale.x);
+            Debug.Log(cellCountInRow);
+
+            cells.AddRange(DrawField(StartPoint.LeftUp, _leftUpCorner, cellCountInRow, cellCountInColumn, cellScale, offsetScale));
+            cells.AddRange(DrawField(StartPoint.RightUp, rightUpCorner, cellCountInRow, cellCountInColumn, cellScale, offsetScale));
+            cells.AddRange(DrawField(StartPoint.LeftDown, leftDownCorner, cellCountInRow, cellCountInColumn, cellScale, offsetScale));
+            cells.AddRange(DrawField(StartPoint.RightDown, _rightDownCorner, cellCountInRow, cellCountInColumn, cellScale, offsetScale));
+
+            int removed = 0;
+
+            List<GameObject> blackList = new();
+
+            foreach (var cell1 in cells)
+            { 
+                if (blackList.Contains(cell1))
+                    continue;
+
+                foreach (var cell2 in cells)
                 {
-                    columns.AddRange(DrowColumn(new Vector2(cell.transform.position.x, RightUpPos.y - (offset.y * i)), cellSize, cellCountInColumn, IsReverse: false));
-                    columns.AddRange(DrowColumn(new Vector2(cell.transform.position.x, LeftDownPos.y + (offset.y * i)), cellSize, cellCountInColumn, IsReverse: true));
+                    if (cell1 != cell2 && cell1.transform.position == cell2.transform.position)
+                    {
+                        if(!blackList.Contains(cell2))
+                            blackList.Add(cell2);
+                    }
                 }
             }
+
+            blackList.ForEach((cell) => { Destroy(cell); removed++;});
+
+            Debug.Log($"Removed: {removed} equal cells");
         }
 
-        private List<GameObject> DrowRow(Vector2 startPosition, Vector2 cellSize, int cellCount, bool IsReverse)
+        enum StartPoint
         {
-            List<GameObject> result = new();
-            for (int i = 0; i < cellCount; i++)
-            {
-                GameObject cellGO = Instantiate(_cellPrefab, _gameArea);
-                cellGO.transform.position = new Vector2(startPosition.x + ((i * cellSize.x) + (cellSize / 2).x) * (IsReverse ? -1 : 1), startPosition.y - (cellSize / 2).y);
-                result.Add(cellGO);
-            }
-            return result;
+            LeftUp,
+            RightUp,
+            LeftDown,
+            RightDown,
         }
-        private List<GameObject> DrowColumn(Vector2 startPosition, Vector2 cellSize, int cellCount, bool IsReverse)
+
+        private List<GameObject> DrawField(StartPoint startPoint, Vector2 startPointValue, int rowCount, int columnCount, Vector2 cellSize, Vector2 offsetSize)
         {
-            List<GameObject> result = new();
-            for (int i = 0; i < cellCount; i++)
+            List<GameObject> spawnedObj = new();
+
+            int param1 = 1;
+            int param2 = 1;
+            int param3 = 1;
+
+            switch (startPoint)
             {
-                GameObject cellGO = Instantiate(_cellPrefab, _gameArea);
-                cellGO.transform.position = new Vector2(startPosition.x, startPosition.y + ((i * cellSize.y) + (cellSize / 2).y) * (IsReverse ? 1 : -1));
-                result.Add(cellGO);
+                case StartPoint.LeftUp:
+                    param1 = 1;
+                    param2 = -1;
+                    param3 = -1;
+                    break;
+                case StartPoint.RightUp:
+                    param1 = -1;
+                    param2 = -1;
+                    param3 = 1;
+                    break;
+                case StartPoint.LeftDown:
+                    param1 = 1;
+                    param2 = 1;
+                    param3 = 1;
+                    break;
+                case StartPoint.RightDown:
+                    param1 = -1;
+                    param2 = 1;
+                    param3 = -1;
+                    break;
             }
-            return result;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                var rowCell = Instantiate(_cellPrefab, transform);
+                rowCell.transform.position = startPointValue + new Vector2(offsetSize.x * i + cellSize.x / 2, cellSize.y / 2 * param3) * param1;
+                spawnedObj.Add(rowCell);
+                for (int j = 1; j < columnCount; j++)
+                {
+                    var columnCell = Instantiate(_cellPrefab, transform);
+                    columnCell.transform.position = (Vector2)rowCell.transform.position + new Vector2(0, offsetSize.y * j) * param2;
+                    spawnedObj.Add(columnCell);
+                }
+            }
+
+            return spawnedObj;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(_leftUpCorner, 0.4f);
+            Gizmos.DrawSphere(_rightDownCorner, 0.4f);
         }
     }
 }
